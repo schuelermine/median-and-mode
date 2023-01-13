33727287bbdcf51531307e5600abf03e64ccb256
Anselm Schüler <mail@anselmschueler.com>
2023-01-14 00:04:27 +0100

    reorganize

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,82 @@
 mod common;
-pub mod median_and_mode;
 mod select_and_iterate;
+
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
+
+use crate::{common::noop, select_and_iterate::select_and_iterate};
+
+#[cfg(test)]
+mod test;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Median<T> {
+    At(T),
+    Between(T, T),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Mode<T: Eq + Hash>(HashSet<T>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct MedianAndMode<T: Eq + Hash> {
+    pub median: Median<T>,
+    pub mode: Mode<T>,
+}
+
+pub fn median_and_mode<T: Ord + Eq + Hash + Clone>(values: &[T]) -> Option<MedianAndMode<T>> {
+    let len = values.len();
+    if len == 0 {
+        return None;
+    }
+    let mut working_values: Vec<&T> = values.iter().collect();
+    let mut frequencies = HashMap::new();
+    let action = |x: &&T| {
+        let frequency = frequencies.entry((*x).clone()).or_insert(0);
+        *frequency += 1;
+    };
+    let median;
+    if len % 2 == 1 {
+        let middle = len / 2;
+        let Some(median_index) = select_and_iterate(&mut working_values, middle, action)
+            else { return None; };
+        median = Median::At(working_values[median_index].clone());
+    } else {
+        let middle_1 = len / 2 - 1;
+        let middle_2 = len / 2;
+        let Some(median_1_index) = select_and_iterate(&mut working_values, middle_1, action)
+            else { return None; };
+        let median_1 = working_values[median_1_index].clone();
+        let Some(median_2_index) = select_and_iterate(&mut working_values, middle_2, noop)
+            else { panic!() };
+        let median_2 = working_values[median_2_index].clone();
+        if median_1 == median_2 {
+            median = Median::At(median_1);
+        } else {
+            median = Median::Between(median_1, median_2);
+        }
+    }
+    let mode = Mode(get_mode(frequencies));
+    Some(MedianAndMode { median, mode })
+}
+
+fn get_mode<T: Eq + Hash>(frequencies: HashMap<T, usize>) -> HashSet<T> {
+    let mut modes = HashSet::new();
+    let mut highest_frequency = 0;
+    for (value, frequency) in frequencies {
+        match frequency.cmp(&highest_frequency) {
+            std::cmp::Ordering::Less => {}
+            std::cmp::Ordering::Equal => {
+                modes.insert(value);
+            }
+            std::cmp::Ordering::Greater => {
+                highest_frequency = frequency;
+                modes.clear();
+                modes.insert(value);
+            }
+        }
+    }
+    modes
+}
--- src/select_and_iterate/test.rs
+++ src/select_and_iterate/test.rs
@@ -1,7 +1,7 @@
-use proptest::{collection::vec, prop_assert_eq, proptest};
-
 use super::select_and_iterate;
 use crate::common::noop;
+use proptest::{collection::vec, prop_assert_eq, proptest};
+use std::collections::HashMap;
 
 #[test]
 fn test_select_empty_vec() {
@@ -12,27 +12,34 @@ fn test_select_empty_vec() {
 
 proptest! {
     #[test]
-    fn test_select(mut values in vec(i128::MIN..i128::MAX, 0..100), index in 0..usize::MAX) {
+    fn test_select(mut values in vec(i8::MIN..i8::MAX, 1..32768), index in 0..usize::MAX) {
         let len = values.len();
         if len == 0 {
             let None = select_and_iterate(&mut values, index, noop)
                 else { panic!() };
         } else {
             let index = index % len;
-            let Some(value_index) = select_and_iterate(&mut values, index, noop)
+            let mut frequencies = HashMap::new();
+            let action = |x: &i8| {
+                let frequency = frequencies.entry(*x).or_insert(0);
+                *frequency += 1;
+            };
+            let Some(value_index) = select_and_iterate(&mut values, index, action)
                 else { panic!() };
             let value = values[value_index];
             values.sort();
             prop_assert_eq!(value, values[index]);
+            for (value, frequency) in frequencies {
+                assert!(values.contains(&value));
+                assert!(frequency <= len);
+            }
         }
     }
 
     #[test]
     fn test_select_singleton_vec(value in i128::MIN..i128::MAX) {
         let mut values = vec![value];
-        let Some(value_index) = select_and_iterate(&mut values, 0, noop)
+        let Some(0) = select_and_iterate(&mut values, 0, noop)
             else { panic!() };
-        let value = values[value_index];
-        prop_assert_eq!(value, values[0])
     }
 }
